013f3f6834139377c756d4fb78d218e74ccc1963
Iggy <igoririanto@rocketmail.com>
2019-10-28 18:31:44 -0500

    WIP - styling

--- src/components/blogs/Blog.vue
+++ src/components/blogs/Blog.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="blog__container">
     <div v-html="devMarkdown"></div>
   </div>
 </template>
@@ -16,10 +16,28 @@ export default {
   computed: {
     devMarkdown: function() {
       const markdownData = fm(devJson[this.$route.params.slug].body_markdown)
-      return `<h1>${markdownData.attributes.title}</h1>
+      return `<h1 class="blog__header">${markdownData.attributes.title}</h1>
       ${md.render(markdownData.body)}`
     },
   }
 }
 </script>
 
+
+<style lang="scss">
+h1 {
+  font-size: 26px;
+  letter-spacing: -0.572px;
+}
+.blog {
+  &__container {
+    display: block;
+    width: 100%;
+    max-width: 645px;
+    margin: 0 auto;
+  }
+  &__header {
+    font-size: 40px;
+  }
+}
+</style>
--- src/components/blogs/Blogs.vue
+++ src/components/blogs/Blogs.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>
-    <h1>All Blogs</h1>
-    <ul>
-      <li v-for="article in devJsonKeys" v-bind:key="article">
-        <router-link :to="`/blog/${article}`">{{devJson[article].title}}</router-link>
+  <div class="blogs__container">
+    <h1 class="blogs__header">All Blogs</h1>
+    <ul class="blogs__list">
+      <li class="blogs__feed" v-for="article in devJsonKeys" v-bind:key="article">
+        <div class="blogs__feed-meta-container">
+          <time>{{devJson[article].published_at}}</time>
+          <span>{{devJson[article].tag_list}}</span>
+        </div>
+        <router-link class="blogs__feed-link" :to="`/blog/${article}`">
+          <h2 class="blogs__feed-title">{{devJson[article].title}}</h2>
+        </router-link>
       </li>
     </ul>
   </div>
@@ -26,6 +32,24 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.blogs {
+  &__feed {
+    list-style-type: none;
+    margin-bottom: 24px;
+  }
+  &__feed-title {
+    font-size: 26px;
+    line-height: 40px;
+    margin-top: 0;
+  }
+  &__feed-link {
+    text-decoration: none;
+    color: #2c3e50;
 
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
 </style>
