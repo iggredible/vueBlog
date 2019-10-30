08693286c2991de03e13dc43f3c167b715ecd21a
Iggy <igoririanto@rocketmail.com>
2019-10-30 17:46:03 -0500

    WIP - add Blogs styling

--- src/components/blogs/Blogs.vue
+++ src/components/blogs/Blogs.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="blogs__container">
     <h1 class="blogs__header">All Blogs</h1>
+    <hr />
     <ul class="blogs__list">
       <li class="blogs__feed" v-for="article in devJsonKeys" v-bind:key="article">
         <div class="blogs__feed-meta-container">
-          <time>{{devJson[article].published_at}}</time>
-          <span>{{devJson[article].tag_list}}</span>
+          <time class="blogs__time">{{readableDate(devJson[article].published_at)}}</time> |
+          <span v-for="tag in devJson[article].tag_list" v-bind:key="tag">
+            <span class="blogs__tag">{{tag}}</span>
+          </span>
         </div>
-        <router-link class="blogs__feed-link" :to="`/blog/${article}`">
-          <h2 class="blogs__feed-title">{{devJson[article].title}}</h2>
-        </router-link>
+        <h2 class="blogs__feed-title">
+          <router-link class="blogs__feed-link" :to="`/blog/${article}`">
+            {{devJson[article].title}}
+          </router-link>
+        </h2>
       </li>
     </ul>
   </div>
@@ -17,6 +22,8 @@
 
 <script>
 import devJson from '../../data/dev/DevTo.json';
+
+const readableDate = (dateStr) => new Date(Date.parse(dateStr));
 
 export default {
   data() {
@@ -28,15 +35,26 @@ export default {
     devJsonKeys() {
       return Object.keys(devJson)
     }
+  },
+  methods: {
+    readableDate(dateStr) {
+      return new Date(Date.parse(dateStr)).toDateString()
+    }
   }
 }
 </script>
 
 <style lang="scss">
 .blogs {
+  &__container {
+    margin: 0 20px;
+  }
+  &__header {
+    /* margin-left: 40px; */
+  }
   &__feed {
     list-style-type: none;
-    margin-bottom: 24px;
+    margin-bottom: 48px;
   }
   &__feed-title {
     font-size: 26px;
@@ -50,6 +68,21 @@ export default {
     &:hover {
       text-decoration: underline;
     }
+  }
+  &__tag {
+    margin: 0 4px;
+    padding: 2px;
+    font-weight: 500;
+    color: #e58e26;
+    display: inline-block;
+    background: #f5f5f5;
+  }
+  &__time {
+    padding: 2px;
+    display: inline-block;
+  }
+  &__list {
+    padding: 0;
   }
 }
 </style>
