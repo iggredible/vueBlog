bd90642eb9bd74e08df70f1222383edf6688aa0f
Iggy <igoririanto@rocketmail.com>
2019-12-07 12:40:30 -0600

    search so far - need to use fuzzysearch lib

--- src/components/sidebar/search/Search.vue
+++ src/components/sidebar/search/Search.vue
@@ -1,9 +1,32 @@
 <template>
-  <input />
+  <div>
+    <input type="text" placeholder="Search article" v-model="search" />
+    {{ filteredArr }}
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      search: "",
+      devArr: []
+    };
+  },
+  created() {
+    this.devArr = this.$store.getters.devJsonArr;
+  },
+  computed: {
+    filteredArr() {
+      console.log("HEY FILTERED ARR");
+      const filteredArr = this.devArr.filter(title => {
+        return title.toLowerCase().includes(this.search.toLowerCase());
+      });
+      console.log(filteredArr);
+      return filteredArr;
+    }
+  }
+};
 </script>
 
 <style></style>
