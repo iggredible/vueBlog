3f4b5daf6e2e6882f486fdf960760ab9384e7e16
Iggy <igoririanto@rocketmail.com>
2019-12-30 17:14:17 -0600

    embed CK only on blog and blogs

--- src/main.js
+++ src/main.js
@@ -11,6 +11,33 @@ Vue.use(VueRouter);
 
 const router = new VueRouter(routes);
 
+router.beforeEach((to, from, next) => {
+  // select DOM element by src https://stackoverflow.com/questions/19252339/get-element-by-src-attribute-with-javascript-not-jquery
+  const ckScript = document.querySelector("script[src*=convertkit]");
+  const ckForm = document.querySelector("form");
+  if (ckForm && (to.name === "Blog" || to.name === "Blogs")) {
+    next();
+  }
+  if (ckScript && (to.name === "Blog" || to.name === "Blogs")) {
+    next();
+  }
+  if (ckForm) document.body.removeChild(ckForm);
+  if (ckScript) document.body.removeChild(ckScript);
+
+  // want to embed ck only on Blog/ BLogs
+  if (to.name === "Blog" || to.name === "Blogs") {
+    let ckEmbed = document.createElement("script");
+    ckEmbed.setAttribute(
+      "src",
+      "https://thoughtful-writer-9767.ck.page/714f9a79e6/index.js"
+    );
+    ckEmbed.setAttribute("data-uid", "714f9a79e6");
+    ckEmbed.async = true;
+    document.body.appendChild(ckEmbed);
+  }
+  next();
+});
+
 const app = new Vue({
   render: h => h(App),
   store,
