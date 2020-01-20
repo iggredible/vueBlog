de6c036c7ba50daa81d0f15ba6a97a6d26e98d4a
Iggy <igoririanto@rocketmail.com>
2020-01-20 13:38:05 -0600

    rmove ck

--- src/main.js
+++ src/main.js
@@ -11,33 +11,6 @@ Vue.use(VueRouter);
 
 const router = new VueRouter(routes);
 
-router.beforeEach((to, from, next) => {
-  // no need to load CK if we are in development. Only do this in production
-  if (process.env.NODE_ENV === "production") {
-    // select DOM element by src https://stackoverflow.com/questions/19252339/get-element-by-src-attribute-with-javascript-not-jquery
-    const ckScript = document.querySelector("script[src*=convertkit]");
-    const ckForm = document.querySelector("form");
-    if ((ckForm || ckScript) && (to.name === "Blog" || to.name === "Blogs")) {
-      next();
-    }
-    if (ckForm) document.body.removeChild(ckForm);
-    if (ckScript) document.body.removeChild(ckScript);
-
-    // want to embed ck only on Blog/ BLogs
-    if (to.name === "Blog" || to.name === "Blogs") {
-      let ckEmbed = document.createElement("script");
-      ckEmbed.setAttribute(
-        "src",
-        "https://thoughtful-writer-9767.ck.page/714f9a79e6/index.js"
-      );
-      ckEmbed.setAttribute("data-uid", "714f9a79e6");
-      ckEmbed.async = true;
-      document.body.appendChild(ckEmbed);
-    }
-  }
-  next();
-});
-
 const app = new Vue({
   render: h => h(App),
   store,
